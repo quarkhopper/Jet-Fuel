9c256ae422eb03500a580b992ed79ffbb0a9ad86
Dan <quarkhopper@gmail.com>
2024-12-13 14:44:35 -0800

    increase blast speed for symmetry

--- script/Init.lua
+++ script/Init.lua
@@ -58,7 +58,7 @@ function createDefaultOptions()
 
 	oSet.blastSpeed = create_mode_option(
 		option_type.numeric, 
-		1,
+		2,
 		"blastSpeed",
 		"Blast speed at detonation (+ for wider initial fireball)")
 	oSet.options[#oSet.options + 1] = oSet.blastSpeed	
