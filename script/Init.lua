04c0b99e11a911b6d2bb85635c4afbce0575839d
Dan <quarkhopper@gmail.com>
2024-12-12 20:30:15 -0800

    tweaks

--- script/Init.lua
+++ script/Init.lua
@@ -156,7 +156,7 @@ function createDefaultOptions()
 
 	oSet.sparkInflateMag = create_mode_option(
 		option_type.numeric, 
-		0.47,
+		0.46,
 		"sparkInflateMag",
 		"Cloud inflation pressure magnitude per spark x 10^-4")
 	oSet.options[#oSet.options + 1] = oSet.sparkInflateMag
